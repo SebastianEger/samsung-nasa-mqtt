eb91e022e6db4126cf85f90a9bf6da041bdf8b25
Sebastian Eger <eger.sebastian@gmx.de>
2025-04-09 10:34:59 +0200

    hotfix

--- docker-run.sh
+++ docker-run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
-docker run --restart unless-stopped \
-    -d --network host samsung-nasa-mqtt:latest --name samsung-nasa-mqtt \
+docker run --restart unless-stopped --name samsung-nasa-mqtt \
+    -d --network host samsung-nasa-mqtt:latest \
     --mqtt-host 192.168.178.2 --serial-port 26 --serial-host 192.168.178.25 --mqtt-port 1883 --nasa-timeout 600 --nasa-pnp
