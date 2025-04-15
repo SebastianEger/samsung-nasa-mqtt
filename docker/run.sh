b1a6e55c2e0b15ee8a45bbe3756f7d8949b375d1
Sebastian Eger <eger.sebastian@gmx.de>
2025-04-15 07:58:56 +0200

    wip

--- docker/run.sh
+++ docker/run.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/bash
 docker run --restart unless-stopped --name samsung-nasa-mqtt \
     -d --network host samsung-nasa-mqtt:latest \
-    --mqtt-host 192.168.178.2 \
-    --serial-port 26 \
     --serial-host 192.168.178.25 \
+    --serial-port 26 \
+    --mqtt-host 192.168.178.2 \
     --mqtt-port 1883 \
-    --nasa-interval 1 \
-    --nasa-timeout 60 \
     --nasa-pnp
